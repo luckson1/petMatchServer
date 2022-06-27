4bbe9dd3e86094cc2c0d8fd7e47c5734d403c105
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-06-28 00:16:09 +0300

    morechanges

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const PORT =process.env.PORT || 5000;
 
 const server= http.createServer(App);
 
-server.listen(PORT, console.log(`server is running on running on port ${5000}`))
+server.listen(PORT, console.log(`server is running on running on port ${PORT}`))
