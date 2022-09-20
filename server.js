18343a0a30dd5fa1fbe4cd0621dbaa6554d9726e
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-20 22:28:40 +0300

    fix: port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./App');
 
 
 
-const PORT =process.env.PORT || 5000;
+const PORT =process.env.PORT || 4000;
 const server= http.createServer(app);
 
 server.listen(PORT, console.log(`server running on port: ${PORT}` ))
