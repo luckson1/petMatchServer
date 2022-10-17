70902cfb95d9b35bd07c1c2bdeeb8622c6398f56
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-10-18 00:18:16 +0300

    fix: increase token expiration

--- middlewear/generateTokens.js
+++ middlewear/generateTokens.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 
 const generateToken= id => {
     
-    return jwt.sign({id}, process.env.JWT_KEY, { expiresIn: '1d'});
+    return jwt.sign({id}, process.env.JWT_KEY, { expiresIn: '14d'});
 };
 
 module.exports =generateToken;
